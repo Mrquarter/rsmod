e48b0457b4abfd005e40502c44ee685ef2f4eea9
Mrquarter <shuman0803@gmail.com>
2019-03-25 12:56:22 -0400

    Lumbridge
    Initial lumbridge design

---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns/shop.kts
@@ -0,0 +1,4 @@
+package gg.rsmod.plugins.content.areas.lumbridge.spawns
+
+spawn_npc(npc = Npcs.SHOP_KEEPER, x = 3212, z = 3248, walkRadius = 4)
+spawn_npc(npc = Npcs.SHOP_ASSISTANT, x = 3209, z =3248, walkRadius = 4)
