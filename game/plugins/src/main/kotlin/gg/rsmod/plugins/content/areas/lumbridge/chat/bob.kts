60c3e615d024b28cafe2bd1b09d30367bffabd84
Mrquarter <shuman0803@gmail.com>
2019-03-26 09:19:19 -0400

    Updated bob and father aereck

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/chat/bob.kts
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/chat/bob.kts
@@ -4,30 +4,44 @@ arrayOf(Npcs.BOB).forEach { shop ->
     }
 
     on_npc_option(npc = shop, option = "trade") {
-        player.queue { open_shop(this) }
+        player.queue { open_shop(this,true) }
     }
 
     on_npc_option(npc = shop, option = "repair") {
-        // TO DO
+        player.queue { repair_items_quick(this) }
     }
 }
 
 suspend fun mainDialog(it: QueueTask) {
-    //it.chatNpc("Can I help you at all?", animation = 567)
     when (it.options("Give me a quest!", "Have you anything to sell?", "Can you repair my items for me?")) {
-        1 -> bobQuest(it)
-        2 -> open_shop(it)
-        3 -> null
+        1 -> bob_quest(it)
+        2 -> open_shop(it, false)
+        3 -> repair_items(it)
     }
 }
 
-suspend fun open_shop(it: QueueTask) {
-    it.chatPlayer("Do you have anything to sell?", animation = 588)
-    it.chatNpc("Yes! I buy and sell axes! Take your pick (or axe)!", animation = 588) // Fix animation
+suspend fun open_shop(it: QueueTask, dialog: Boolean) {
+    if (dialog) {
+        it.chatPlayer("Have you anything to sell?", animation = 588)
+        it.chatNpc("Yes! I buy and sell axes! Take your pick (or axe)!", animation = 588) // Fix animation
+    }
     it.player.openShop("Bob's Brilliant Axes")
 }
 
-suspend fun bobQuest(it: QueueTask) {
+suspend fun bob_quest(it: QueueTask) {
     it.chatPlayer("Give me a quest!", animation = 588)
     it.chatNpc("Get yer own!")
+}
+
+suspend fun repair_items(it: QueueTask) {
+    it.chatPlayer("Can you repair my items for me?", animation = 588)
+    it.chatNpc("Of course I'll repair it, though the materials may cost you. Just hand me the item and I'll have a look.")
+}
+
+suspend fun repair_items_quick(it: QueueTask) {
+    it.chatNpc("You don't have anything I can repair")
+    /**
+     * To Do
+     * Build in repairing items functionality
+     */
 }
---
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/areas/lumbridge/spawns/father_aereck.kts
@@ -0,0 +1,3 @@
+package gg.rsmod.plugins.content.areas.lumbridge.spawns
+
+spawn_npc(npc = Npcs.FATHER_AERECK, x = 3244, z = 3209, walkRadius = 5)
